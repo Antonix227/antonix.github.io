188bc614c2b6dcf25def147a40e3c18e99b0df13
Antonix227 <63429101+Antonix227@users.noreply.github.com>
2023-10-08 22:27:59 +0400

    Add files via upload

--- zadanie13/script.js
+++ zadanie13/script.js
@@ -2,6 +2,10 @@ $(".button").click(function(){
     let srcImg = ($('.zdj').attr("src") === "przed.png")
                 ? "po.png" 
                 : "przed.png";
-$(".photo").toggleClass("yellow-background") ;               
+    let info = ($(".info").html() === "Przed")
+                ? "Po" 
+                : "Przed";
+$(".photo").toggleClass("yellow-background") ;
+$(".info").html(info)               
  $(".zdj").attr('src',srcImg )  ; 
 })
